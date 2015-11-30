33a69f31c022201a8abf9411f4f62d81aaba044b
Alex Griciuc <agriciuc@gmail.com>
2015-11-30 15:13:16 +0200

    finished script

--- index.js
+++ index.js
@@ -6,17 +6,6 @@ import {MailChimpAPI} from "mailchimp";
 import _ from 'lodash';
 
 const mailchimpApiKey = process.env.MAILCHIMP_API_KEY; 
-const testContact = {
-  email: {
-    email: 'agriciuc@gmail.com' 
-  }, 
-  email_type: 'html', 
-  merge_vars: { 
-    EMAIL: 'agriciuc@gmail.com', 
-    FNAME: 'Alex',
-    LNAME: 'GRICIUC'
-  }
-};
 
 let mailchimpApi;
 try {
@@ -26,23 +15,43 @@ try {
 }
 
 
-vdata.fromFile(__dirname + "/contacts-test.vcf", (err, res) => {
+vdata.fromFile(__dirname + "/contacts.vcf", (err, res) => {
   if (err) throw err;
 
+  const contacts = _.map(res.VCARD, createMailchimpContact);
   mailchimpApi.call('lists', 'list', {start: 0, limit: 25}, (err, lists) => {
     if (err) throw err;
 
     const firstListId = _.result(_.findWhere(lists.data, {'name': 'First list'}), 'id');
-    console.log(firstListId);
+    importContacts(firstListId, contacts, (err, result) => {
+      if (err) throw err;
+
+      console.log(result);
+    });
   });
 });
+
+const createMailchimpContact = (vdataContact) => {
+  if (validator.isEmail(vdataContact.EMAIL.value)) {
+    return {
+      email: {
+        email: vdataContact.EMAIL.value 
+      },
+      email_type: 'html',
+      merge_vars: {
+        EMAIL: vdataContact.EMAIL.value,
+        FNAME: vdataContact.FN
+      }
+    };
+  }
+};
 
 const importContacts = (id, contacts, next) => {
   mailchimpApi.call('lists', 'batch-subscribe', {
       id: id, 
       batch: contacts, 
       double_optin: false, 
-      update_existing: false, 
+      update_existing: true, 
       replace_interests: false
     }, (err, res) => {
       if (err) next(err);
