427b6d6bdc6033667b080890e1f52c50d2a78f68
Alex Griciuc <agriciuc@outlook.com>
2016-03-03 15:46:38 +0200

    updated with new list

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import validator from "validator";
 import {MailChimpAPI} from "mailchimp";
 import _ from 'lodash';
 
-const mailchimpApiKey = process.env.MAILCHIMP_API_KEY; 
 
+const mailchimpApiKey = '384d368ed7cb184cdbb4fedec5ca235c-us12';
 let mailchimpApi;
 try {
   mailchimpApi = new MailChimpAPI(mailchimpApiKey, {version: '2.0'});
@@ -14,28 +14,11 @@ try {
   console.error(e);
 }
 
-
-vdata.fromFile(__dirname + "/contacts.vcf", (err, res) => {
-  if (err) throw err;
-
-  const contacts = _.map(res.VCARD, createMailchimpContact);
-  mailchimpApi.call('lists', 'list', {start: 0, limit: 25}, (err, lists) => {
-    if (err) throw err;
-
-    const firstListId = _.result(_.findWhere(lists.data, {'name': 'First list'}), 'id');
-    importContacts(firstListId, contacts, (err, result) => {
-      if (err) throw err;
-
-      console.log(result);
-    });
-  });
-});
-
 const createMailchimpContact = (vdataContact) => {
   if (validator.isEmail(vdataContact.EMAIL.value)) {
     return {
       email: {
-        email: vdataContact.EMAIL.value 
+        email: vdataContact.EMAIL.value
       },
       email_type: 'html',
       merge_vars: {
@@ -48,13 +31,32 @@ const createMailchimpContact = (vdataContact) => {
 
 const importContacts = (id, contacts, next) => {
   mailchimpApi.call('lists', 'batch-subscribe', {
-      id: id, 
-      batch: contacts, 
-      double_optin: false, 
-      update_existing: true, 
+      id: id,
+      batch: contacts,
+      double_optin: false,
+      update_existing: true,
       replace_interests: false
     }, (err, res) => {
       if (err) next(err);
       next(null, res);
   });
 };
+
+console.log(mailchimpApi);
+vdata.fromFile(__dirname + "/contacts.vcf", (err, res) => {
+  if (err) throw err;
+
+  const contacts = _.map(res.VCARD, createMailchimpContact);
+  mailchimpApi.call('lists', 'list', {start: 0, limit: 25}, (err, lists) => {
+    if (err) throw err;
+
+    const firstListId = _.result(_.findWhere(lists.data, {'name': 'Contacte martie'}), 'id');
+    importContacts(firstListId, contacts, (err, result) => {
+      if (err) throw err;
+
+      console.log(result);
+    });
+  });
+});
+
+
